1b5e96da609992076fccc7c4cae9b1e21ab7571e
nd <nitin.rajen.deshmukh@gmail.com>
2018-04-15 13:45:17 +0200

    Added Lambda handler function for inserting SNS message into DynamoDB table

---
+++ index.js
@@ -0,0 +1,31 @@
+const AWS = require("aws-sdk");
+const uuid = require("uuid");
+AWS.config.update({region: "us-west-2"});
+
+exports.handler = async (event) => {
+    try {
+        // Extract message
+        const message = JSON.parse(event.Records[0].Sns.Message);
+
+        // Prepare account item
+        const accountItem = {
+            TableName: "Account",
+            Item: {
+                id: uuid.v1(),
+                name: message.name,
+                iban: message.iban
+            }
+        };
+
+        // Create DynamoDB document client
+        const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+        // insert account in DynamoDB
+        await dynamoDb.put(accountItem).promise();
+
+        return "Registered new account with id " + accountItem.Item.id;
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
+}
